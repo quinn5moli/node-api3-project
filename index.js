3215db5994390acd9aa49ac41b4440c519485d2f
quinn5moli <quinn.moli@gmail.com>
2021-11-01 19:54:53 -0700

    server working, dependences added, connected users-router

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it
+const server = require('./api/server')
+
+const PORT = 4040;
+
+server.listen(PORT, () => {
+    console.log(`\n**server running on port ${PORT}**`
+    .america)
+})
