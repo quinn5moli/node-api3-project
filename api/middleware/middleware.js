665c0e14af78b5ca8776aee7c2323593660c3e76
quinn5moli <quinn.moli@gmail.com>
2021-11-02 19:21:08 -0700

    scaffolding and rough draft of middleware made; logger imported to server

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,50 @@
+const model = require('../users/users-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log(`[${req.method}] to ${req.url} at ${Date()}`)
+  next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
-}
+// function validateUserId(req, res, next) {
+//   // DO YOUR MAGIC
+//   console.log('validateUserId')
+//   model.getById(req.params.id)
+//     .then(user => {
+//       if (!user) {
+//         res.status(404).json({message: 'user not found' })
+//       } else {
+//         req.user = user
+//         next()
+//       }
+//     })
 
-function validateUser(req, res, next) {
-  // DO YOUR MAGIC
-}
+// }
 
-function validatePost(req, res, next) {
-  // DO YOUR MAGIC
-}
+// function validateUser(req, res, next) {
+//   // DO YOUR MAGIC
+//   console.log('validateUser')
+//   if(!req.body.name) {
+//       res.status(400).json({ message: 'missing required name field'})
+//   } else {
+//     next()
+//   }
+// }
+
+// function validatePost(req, res, next) {
+//   // DO YOUR MAGIC
+//   console.log('validatePost')
+//   if(!req.body.text) {
+//     res.status(400).json({ message: 'missing required text field' })
+//   } else {
+//     next()
+//   }
+// }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  // validateUserId,
+  // validateUser,
+  // validatePost
+};
--- api/server.js
+++ api/server.js
@@ -1,20 +1,18 @@
 const express = require('express');
-const cors = require('cors');
-const model = require('./users/users-model');
-require('colors');
-
-const userRoutes = require('./users/users-router');
-
+require('colors')
+const userRouter = require('./users/users-router');
+const { logger } = require('./middleware/middleware');
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
-server.use(express.json())
+server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
-server.use(cors())
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+server.use('/users', logger, userRouter);
+
+server.use('/', logger, (req, res) => {
+  res.status(200).send('base route')
 });
 
 module.exports = server;
